3c1f42ca9c4228ca22fd30e2db44a64c97ab3043
emmanyouwell <emingala02@gmail.com>
2024-08-05 11:19:17 +0800

    Get statuses included

--- routes/device.js
+++ routes/device.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const {loginDevice, healthCheck, getAllDevices} = require("../controllers/deviceController.js");
 
-router.post("/device/login/:id", loginDevice);
+// router.post("/device/login/:id", loginDevice);
 router.get("/device/statuses", getAllDevices);
-router.get("/device/update-status", healthCheck);
+// router.get("/device/update-status", healthCheck);
 module.exports = router;
