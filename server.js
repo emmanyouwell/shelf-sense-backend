7526c89109ea2502cca163bdf3007037046c853e
emmanyouwell <emingala02@gmail.com>
2025-07-02 12:04:22 +0800

    - Removed node_modules in git tracking - Cleaned code smells

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 const app = require('./app');
 const connectDatabase = require('./config/database')
-const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config({path: './config/config.env'})
 
 connectDatabase();
 
-// function checkStatuses() {
-// 	fetch('https://shelf-sense-backend.onrender.com/api/v1/device/update-status')
-// 	  .then(response => response.json())
-// 	  .then(data => {
-// 		console.log('Statuses updated:', data);
-// 	  })
-// 	  .catch(error => {
-// 		console.error('Error updating statuses:', error);
-// 	  });
-//   }
-  
-//   // Call checkStatuses every minute
-//   setInterval(checkStatuses, 5000); // 60000 ms = 1 minute
-  
-//   // Optionally, call it immediately on startup
-//   checkStatuses();
 app.listen(process.env.PORT, () => {
 	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
